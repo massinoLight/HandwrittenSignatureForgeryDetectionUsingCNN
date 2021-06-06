1576d17598e818da004503f10ab9a0b418ca1449
massinoLight <nacerddine.amassin@gmail.com>
2021-06-06 03:29:11 +0200

    commit la pression a chaque point est récup au format JSON

--- SignaturePad_JS_Flask/static/js/pression.js
+++ SignaturePad_JS_Flask/static/js/pression.js
@@ -1,22 +1,82 @@
 var startTime;
 
+var saveData = (function () {
+        var a = document.createElement("a");
+        document.body.appendChild(a);
+        a.style = "display: none";
+        return function (data, fileName) {
+            var json = JSON.stringify(data),
+                blob = new Blob([json], {type: "octet/stream"}),
+                url = window.URL.createObjectURL(blob);
+            a.href = url;
+            a.download = fileName;
+            a.click();
+            window.URL.revokeObjectURL(url);
+        };
+    }());
+var data ;
+var myJSON;
+var xpos;
+var ypos;
+
+
+function findScreenCoords(mouseEvent)
+{
+
+  if (mouseEvent)
+  {
+    //FireFox
+    xpos = mouseEvent.screenX;
+    ypos = mouseEvent.screenY;
+  }
+  else
+  {
+    //IE
+    xpos = window.event.screenX;
+    ypos = window.event.screenY;
+  }
+  return xpos,ypos
+  //console.log("X",xpos," Y",ypos);
+}
+
+
+
+
+
+
+
 
 Pressure.set('#el1', {
   start: function(event){
     startTime = new Date().getTime();
 
 
-    console.log('X', event.clientX,'Y', event.clientY,'Pression', event.pressure);
+    document.getElementById("signature-pad").onmousemove = findScreenCoords;
+
+     data = { X: event.clientX, Y: event.clientY, Pression: event.pressure };
+     myJSON = JSON.stringify(data);
+    //console.log('JSON',myJSON);
+    //saveData(data, fileName);
+    //console.log('File saved');
+
   },
   end: function(){
     var endTime = new Date().getTime();
 
      console.log((endTime-startTime),'ms');
+     myJSON=myJSON+"temps(ms):"+(endTime-startTime)
+     console.log('JSON',myJSON);
 
   },
 
   change: function(force, event){
-    console.log('Pression',force);
+
+    document.getElementById("signature-pad").onmousemove = findScreenCoords;
+
+    console.log("X",xpos," Y",ypos," Pression",force);
+    data = { X: xpos, Y: ypos, Pression: force };
+    temp = JSON.stringify(data);
+    myJSON=myJSON+ "\n" +temp;
 
   },
   unsupported: function(){
